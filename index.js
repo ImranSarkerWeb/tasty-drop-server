67e31c6f255c5439b25cbc16897c610ddd129a13
Naimur Reza <naimur.rezaa@gmail.com>
2023-09-06 11:46:54 +0600

    added: updated code

--- index.js
+++ index.js
@@ -178,7 +178,7 @@ async function run() {
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
-    app.get("/users", verifyJwt, async (req, res) => {
+    app.get("/users", async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });

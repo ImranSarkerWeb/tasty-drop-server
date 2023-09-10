03ddea6005da22dd48fcb57e30192352dedca495
imransarkerweb <imransarker.web@gmail.com>
2023-09-10 23:07:59 +0600

    local updated

--- index.js
+++ index.js
@@ -219,7 +219,12 @@ async function run() {
           return res.status(404).json({ error: "Menu item not found" });
         }
 
-        res.status(200).json({ message: "Menu item deleted successfully!", deletedItem: foundMenuItem._id });
+        res
+          .status(200)
+          .json({
+            message: "Menu item deleted successfully!",
+            deletedItem: foundMenuItem._id,
+          });
       } catch (error) {
         console.error("Error deleting menu item:", error);
         res.status(500).json({ error: "Internal server error" });
@@ -404,13 +409,13 @@ async function run() {
         ship_postcode: 1000,
         ship_country: "Bangladesh",
       };
-    
+
       const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live);
       sslcz.init(data).then(async (apiResponse) => {
         // Redirect the user to payment gateway
         let GatewayPageURL = apiResponse.GatewayPageURL;
         res.send({ url: GatewayPageURL });
-        
+
         const query = { _id: new ObjectId(id) };
         const findResturent = await partnerCollection.findOne(query);
         orderData._id = new ObjectId();
@@ -459,7 +464,7 @@ async function run() {
         const tranId = req.params.tranId;
         const result = await partnerCollection.updateOne(
           { "order.tranjectionId": tranId },
-          { $pull: { "order": { tranjectionId: tranId } } }
+          { $pull: { order: { tranjectionId: tranId } } }
         );
         if (result.modifiedCount > 0) {
           res.redirect(`http://localhost:5173/payment/fail`);

78d5f50b5411c9db61aee1de68e6bba319691d6f
BakiAbdullah <shatil605@gmail.com>
2023-09-19 14:37:32 +0600

    commented out await client connect line 59

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const usersCollection = client.db("tastyDB").collection("users");
     const reviewCollection = client.db("tastyDB").collection("reviews");
